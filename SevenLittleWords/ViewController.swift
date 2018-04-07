cb6bb32b9996db66ce98d6b72460f0d4c2242287
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-06 17:17:48 -0700

    Added preliminary UI structure and connected all the necessary outlets and actions.

--- SevenLittleWords/ViewController.swift
+++ SevenLittleWords/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
